d737949f6ace9f11d90dabfff4c228f96699ec3b
Den Lytvynenko <windy228666@gmail.com>
2025-09-27 10:52:58 +0300

    Created Category Table

--- CoSeClothes/Data/ApplicationDbContext.cs
+++ CoSeClothes/Data/ApplicationDbContext.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CoSeClothes.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoSeClothes.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-            
-        }
+        {}
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
